8c396d665afd1c26a2b1b6f100b64c4a4ce2d4e6
José Anderson da Silva Costa <60760405+DessimA@users.noreply.github.com>
2022-11-25 19:49:35 -0300

    IDJD-spriteMove
    Aula de movimento com sprite

--- IDJD - Projeto Corrida/Carro-02.gd
+++ IDJD - Projeto Corrida/Carro-02.gd
@@ -3,7 +3,7 @@ extends Node2D
 var vel = 200
 func _physics_process(delta):
 	var direcao = Vector2()
-	
+	yield(get_tree().create_timer(1), "timeout")
 	direcao = Vector2(0, -1)		
 		
 	translate(direcao * vel * delta)
